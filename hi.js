ede635cadd1f3567f44ee91f69b65b6d085168a7
Ritika Revankar <138849917+ritu2425@users.noreply.github.com>
2023-09-24 17:20:40 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,12 @@
+// strt.addEventListener("click",function(){
+//     content.style.animation=" animate 4s ease-in-out infinite";
+// });
+// stp.addEventListener("click",function(){
+//     content.style.animation=" ";
+// });
+function gotoLink(){
+ document.location.href="project.html";
+}
+function gotoLinked(){
+    document.location.href="index.html";  
+}
